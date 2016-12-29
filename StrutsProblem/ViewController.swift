3cc559fd56da8c5b8a7da814bca3b405698fe25a
Bill Heitzeg <william.heitzeg@tdameritrade.com>
2016-12-29 13:31:10 -0500

    Layout working for all views except upside down portrait

--- StrutsProblem/ViewController.swift
+++ StrutsProblem/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLeftView: UIImageView!
+    @IBOutlet weak var topRightView: UIImageView!
+    @IBOutlet weak var bottomView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
